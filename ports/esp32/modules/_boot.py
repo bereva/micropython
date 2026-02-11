eeb48769b58f2045c89f23d797a6790d72938afb
Lorenzo Bolognese <57444324+lorenzobolognese@users.noreply.github.com>
2026-02-11 16:47:38 +0100

    Add bypass file 'xcore.auth' to force break execution

--- ports/esp32/modules/_boot.py
+++ ports/esp32/modules/_boot.py
@@ -5,8 +5,10 @@ from time import sleep_ms
 from sys import exit
 from micropython import kbd_intr
 from machine import RTC
+from os import stat
 
-__break_wait_time_seconds = 3
+__BREAK_WAIT_TIME_SECONDS = 3
+__FORCE_BREAK_FILENAME = 'xcore.break'
 
 try:
     if bdev:
@@ -32,18 +34,25 @@ if bool('b R e A k B y P a S s' not in RTC().memory().decode()) == True:
     kbd_intr(3)
     
     try:
-        while __break_wait_time_seconds > 0:
-            print("Hit CTRL-C to break execution (" + str(__break_wait_time_seconds) + " sec. left)...")
+        # Check for break-force file in the filesystem
+        try: stat(__FORCE_BREAK_FILENAME)
+        except OSError:
+            print(__FORCE_BREAK_FILENAME + " file found: breaking execution...")
+            raise KeyboardInterrupt
+        
+        # Start countdown
+        while __BREAK_WAIT_TIME_SECONDS > 0:
+            print("Hit CTRL-C to break execution (" + str(__BREAK_WAIT_TIME_SECONDS) + " sec. left)...")
             sleep_ms(1000)
-            __break_wait_time_seconds = __break_wait_time_seconds - 1
+            __BREAK_WAIT_TIME_SECONDS = __BREAK_WAIT_TIME_SECONDS - 1
     except KeyboardInterrupt:
 
         # Delete all allocated objects
-        del __break_wait_time_seconds, sleep_ms, kbd_intr, RTC    
+        del __BREAK_WAIT_TIME_SECONDS, __FORCE_BREAK_FILENAME, sleep_ms, kbd_intr, RTC, stat
         exit()
 
     # Restore CTRL-C (disabled)
     kbd_intr(-1)
     
 # Delete allocated objects
-del __break_wait_time_seconds, sleep_ms, exit, kbd_intr, RTC    
+del __BREAK_WAIT_TIME_SECONDS, __FORCE_BREAK_FILENAME, sleep_ms, exit, kbd_intr, RTC, stat
